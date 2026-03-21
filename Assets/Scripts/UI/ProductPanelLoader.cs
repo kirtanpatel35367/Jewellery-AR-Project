51cf40469b53e19ab481708eb1027a8332a641fb
Kirtan Patel <kp534422@gmail.com>
2026-03-21 23:16:57 +0530

    Revert "Merge branch 'zuli-ui' of https://github.com/kirtanpatel35367/Jewellery-AR-Project into kirtan-ui-merged"
    This reverts commit eb7fb55ee86130bea6ce89d973e879897596f0b5, reversing
    changes made to 549f4d92f2950054c48e17454d3447b036ee7152.

--- Assets/Scripts/UI/ProductPanelLoader.cs
+++ Assets/Scripts/UI/ProductPanelLoader.cs
@@ -1,87 +1,3 @@
-// using UnityEngine;
-// using TMPro;
-// using UnityEngine.UI;
-
-// public class ProductPanelLoader : MonoBehaviour
-// {
-//     [Header("UI References")]
-//     public Transform productGrid;
-//     public GameObject productCardPrefab;     
-//     public TextMeshProUGUI titleText;
-
-//     private void OnEnable()
-//     {
-//         LoadProducts();
-//     }
-
-//     public void LoadProducts()
-//     {
-//         if (SelectedCategoryStore.Instance == null)
-//         {
-//             Debug.LogWarning("SelectedCategoryStore instance not found.");
-//             return;
-//         }
-
-//         string category = SelectedCategoryStore.Instance.selectedCategory;
-
-//         if (string.IsNullOrEmpty(category))
-//         {
-//             Debug.LogWarning("No category selected.");
-//             return;
-//         }
-
-//         if (titleText != null)
-// {
-//     titleText.text = category;
-//     Debug.Log("Title updated to: " + titleText.text);
-// }
-
-//         ClearGrid();
-
-//         Sprite[] thumbnails = Resources.LoadAll<Sprite>("Thumbnails/" + category);
-
-//         if (thumbnails == null || thumbnails.Length == 0)
-//         {
-//             Debug.LogWarning("No thumbnails found in Resources/Thumbnails/" + category);
-//             return;
-//         }
-
-//         foreach (Sprite thumb in thumbnails)
-//         {
-//             GameObject card = Instantiate(productCardPrefab, productGrid);
-
-//             Transform imageTf = card.transform.Find("ProductImage");
-//             Transform nameTf = card.transform.Find("ProductName");
-
-//             if (imageTf != null)
-//             {
-//                 Image img = imageTf.GetComponent<Image>();
-//                 if (img != null)
-//                 {
-//                     img.sprite = thumb;
-//                     img.preserveAspect = true;
-//                 }
-//             }
-
-//             if (nameTf != null)
-//             {
-//                 TextMeshProUGUI txt = nameTf.GetComponent<TextMeshProUGUI>();
-//                 if (txt != null)
-//                 {
-//                     txt.text = thumb.name;
-//                 }
-//             }
-//         }
-//     }
-
-//     private void ClearGrid()
-//     {
-//         for (int i = productGrid.childCount - 1; i >= 0; i--)
-//         {
-//             Destroy(productGrid.GetChild(i).gameObject);
-//         }
-//     }
-// }
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
@@ -92,10 +8,6 @@ public class ProductPanelLoader : MonoBehaviour
     public Transform productGrid;
     public GameObject productCardPrefab;
     public TextMeshProUGUI titleText;
-
-    [Header("Panels")]
-    public GameObject productPanel;
-    public GameObject viewer3DPanel;
 
     private void OnEnable()
     {
@@ -118,56 +30,52 @@ public class ProductPanelLoader : MonoBehaviour
             return;
         }
 
-        category = category.Trim();
-
         if (titleText != null)
-            titleText.text = category;
+{
+    titleText.text = category;
+    Debug.Log("Title updated to: " + titleText.text);
+}
 
         ClearGrid();
 
-        string path = "Thumbnails/" + category;
-        Sprite[] thumbnails = Resources.LoadAll<Sprite>(path);
-
-        Debug.Log("Loading thumbnails from Resources/" + path);
-        Debug.Log("Found thumbnails count = " + thumbnails.Length);
+        Sprite[] thumbnails = Resources.LoadAll<Sprite>("Thumbnails/" + category);
 
         if (thumbnails == null || thumbnails.Length == 0)
         {
-            Debug.LogWarning("No thumbnails found in Resources/" + path);
+            Debug.LogWarning("No thumbnails found in Resources/Thumbnails/" + category);
             return;
         }
 
         foreach (Sprite thumb in thumbnails)
         {
             GameObject card = Instantiate(productCardPrefab, productGrid);
-            card.name = "Card_" + thumb.name;
 
-            ProductCardItem cardItem = card.GetComponent<ProductCardItem>();
-            if (cardItem == null)
+            Transform imageTf = card.transform.Find("ProductImage");
+            Transform nameTf = card.transform.Find("ProductName");
+
+            if (imageTf != null)
             {
-                cardItem = card.AddComponent<ProductCardItem>();
+                Image img = imageTf.GetComponent<Image>();
+                if (img != null)
+                {
+                    img.sprite = thumb;
+                    img.preserveAspect = true;
+                }
             }
 
-            cardItem.Setup(thumb, productPanel, viewer3DPanel);
-
-            Button btn = card.GetComponent<Button>();
-            if (btn != null)
+            if (nameTf != null)
             {
-                btn.onClick.RemoveAllListeners();
-                btn.onClick.AddListener(cardItem.OnCardClicked);
-            }
-            else
-            {
-                Debug.LogError("Button missing on ProductCard prefab root.");
+                TextMeshProUGUI txt = nameTf.GetComponent<TextMeshProUGUI>();
+                if (txt != null)
+                {
+                    txt.text = thumb.name;
+                }
             }
         }
     }
 
     private void ClearGrid()
     {
-        if (productGrid == null)
-            return;
-
         for (int i = productGrid.childCount - 1; i >= 0; i--)
         {
             Destroy(productGrid.GetChild(i).gameObject);
--- Assets/Scripts/UI/SelectedCategoryStore.cs
+++ Assets/Scripts/UI/SelectedCategoryStore.cs
@@ -1,30 +1,3 @@
-// using UnityEngine;
-
-// public class SelectedCategoryStore : MonoBehaviour
-// {
-//     public static SelectedCategoryStore Instance;
-
-//     [Header("Current Selection")]
-//     public string selectedCategory;
-
-//     private void Awake()
-//     {
-//         if (Instance == null)
-//         {
-//             Instance = this;
-//         }
-//         else
-//         {
-//             Destroy(gameObject);
-//         }
-//     }
-
-//     public void SetCategory(string categoryName)
-//     {
-//         selectedCategory = categoryName;
-//         Debug.Log("Selected Category: " + selectedCategory);
-//     }
-// }
 using UnityEngine;
 
 public class SelectedCategoryStore : MonoBehaviour
@@ -36,13 +9,14 @@ public class SelectedCategoryStore : MonoBehaviour
 
     private void Awake()
     {
-        Instance = this;
-        Debug.Log("SelectedCategoryStore Awake.");
-    }
-
-    private void OnEnable()
-    {
-        Instance = this;
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
     public void SetCategory(string categoryName)
